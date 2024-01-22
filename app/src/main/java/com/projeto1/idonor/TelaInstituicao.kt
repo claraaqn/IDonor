0bd5a8bc0e5b60e46295474d29f10eb2db950b7f
amjvidal <136771834+amjvidal@users.noreply.github.com>
2024-01-22 17:16:37 -0300

    Telas do perfil da instituição

--- app/src/main/java/com/projeto1/idonor/TelaInstituicao.kt
+++ app/src/main/java/com/projeto1/idonor/TelaInstituicao.kt
@@ -7,7 +7,7 @@ import android.os.Bundle
 class TelaInstituicao : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.tela_instituicao)
+        setContentView(R.layout.tela_instituicao_info)
 
     }
 }
