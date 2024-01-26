73cc34fda00b7b8d90540b8ce32fda72c779a4db
amjvidal <136771834+amjvidal@users.noreply.github.com>
2024-01-26 07:40:51 -0300

    Feito as ligações de todas as telas e alguns ajustes

--- app/src/main/java/com/projeto1/idonor/TelaConfirmacao.kt
+++ app/src/main/java/com/projeto1/idonor/TelaConfirmacao.kt
@@ -1,11 +1,34 @@
 package com.projeto1.idonor
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class TelaConfirmacao : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.tela_confirmacao)
+
+        var btnVoltar = findViewById<Button>(R.id.botaovoltarconfirmacao)
+        var btnDoar = findViewById<Button>(R.id.botaodoarconfirmacao)
+
+        fun irEspecificacao(view: View){
+            var intent = Intent(this, TelaEspecificacao::class.java)
+            startActivity(intent)
+        }
+        btnVoltar.setOnClickListener{
+            irEspecificacao(it)
+        }
+
+        fun irAgradecimento(view: View){
+            var intent = Intent(this, TelaAgradecimentoActivity::class.java)
+            startActivity(intent)
+        }
+        btnDoar.setOnClickListener{
+            irAgradecimento(it)
+        }
+
     }
 }
