a943e6a534c11d99534c4a3ba3d09ad3ade565b0
amjvidal <136771834+amjvidal@users.noreply.github.com>
2024-01-26 02:36:59 -0300

    Feitos as Telas de Confirmação e Especificação, 2 java class criadas para testes posteriores e algumas mudancinhas

--- app/src/main/java/com/projeto1/idonor/TelaEspecificacao.kt
+++ app/src/main/java/com/projeto1/idonor/TelaEspecificacao.kt
@@ -2,10 +2,21 @@ package com.projeto1.idonor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 
 class TelaEspecificacao : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.tela_especificacao)
+
+
     }
 }
+
+
+
